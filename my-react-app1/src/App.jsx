0044a9fa3c699889baec94af52bbcc6cd589386f
yuxzi <kitty841002@gmail.com>
2024-11-12 16:19:30 +0800

    1112與css做結合，熟練使用組件

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,37 +1,90 @@
-//傳遞組件
-// 把組件放到另一個組件的內容裡面
-// 這種寫法在react中叫做組合(composition)
-// 此方法在不影響組件功能的基礎上，另外再給組件加入額外的功能
-//建立一個新的組件
-function SecondComponent() {
-  return <h2>我是 SecondComponent</h2>;
-}
+import "./app.css";
 
-//子組件
-function MyComponent({ children }) {
-  // return<>
-  // {children}
-  // </>
-
-  // 兩個組件共同出現
-  return (
-    <>
-      <h1>我是MyComponent</h1>
-      {children}
-    </>
-  );
-}
-// 父組件
 function App() {
   return (
     <>
-      {/* 組件裡面呼叫組件 */}
-      {/* <MyComponent children={<SecondComponent/>}/> =>原本的寫法，因為children是內建屬性所以可以不用這樣寫*/}
+      <header>
+        <div>
+          <img src="./images/banner.jpg" />
+        </div>
+      </header>
 
-      {/* app父=>MyComponent子, MyComponent變成父=>SecondComponent子//(父傳子)*/}
-      <MyComponent>
-        <SecondComponent />
-      </MyComponent>
+      <main>
+        {/*文字區 */}
+        <div className="textBlock">
+          <div>
+            <h2>教學影音</h2>
+            <p>
+              誘導影片課程，來了解軟體的核心概念以及入門，
+              並跟著範例練習以了解其中之應用方式
+            </p>
+          </div>
+          <div>
+            <h2>良性互動</h2>
+            <p>
+              學習過程中，若您得到任何問題或是您有任何想法都歡迎
+              提出，我們會盡量的協助問題的解決
+            </p>
+          </div>
+          <div>
+            <h2>趨勢分享</h2>
+            <p>分享目前時下所流行之趨勢，讓各位可隨時掌握新趨勢的發展</p>
+          </div>
+        </div>
+
+        {/* 主標 */}
+        <div className="item">
+          <h1>讓學習是一種習慣</h1>
+        </div>
+
+        {/* 課程 */}
+        <div className="class">
+          <h2>推薦課程</h2>
+          <div className="classBox">
+            <div className="content">
+              <img src="./images/unity.jpg" alt="" />
+
+              <h2>Unity 5</h2>
+              <p className="p1">最新的Unity公開課程，讓你一步一步做出自己的心中理想的遊戲</p>
+              <p>講師:xxx</p>
+              <p>影音課程時數:4小時</p>
+              <p><span className="span1">原價NT$1600</span><span className="span2">NT$1200</span></p>
+            
+              <button>付款上課去</button>
+            </div>
+
+            <div className="content">
+              <img src="./images/gamesalad.jpg" alt="" />
+
+              <h2>GameSalad 2D遊戲製作</h2>
+              <p className="p1">視覺化操作，2D遊戲快速開發，不會寫程式的你一樣可以寫遊戲</p>
+              <p>講師:xxx</p>
+              <p>影音課程時數:8小時</p>
+              <p><span className="span1">原價NT$3200</span><span className="span2">NT$1600</span></p>
+              <button>付款上課去</button>
+            </div>
+            <div className="content">
+              <img src="./images/gwd.jpg" alt="" />
+
+              <h2>Google Web Designer</h2>
+              <p className="p1">
+                用於建立HTML5廣告動畫，編輯時間軸，
+                將各種物件和事件以動畫方式呈現
+              </p>
+              <p>講師:xxx</p>
+              <p>影音課程時數:8小時</p>
+              <p><span className="span1">原價NT$3200</span><span className="span2">NT$1600</span></p>
+              <button>付款上課去</button>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer>
+        <div>
+          <p>&copy;2024/11/12陳姿佑</p>
+        </div>
+      </footer>
     </>
   );
 }
