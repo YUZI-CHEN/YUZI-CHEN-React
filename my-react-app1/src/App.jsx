c47450db38e71e4ead758bde2531f1e1ea3eb4e4
yuxzi <kitty841002@gmail.com>
2024-11-12 16:49:31 +0800

    組件練習

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,6 +1,41 @@
 import "./app.css";
 
 function App() {
+  const textBox = {
+    s1: {
+      name: "教學影音",
+      contents:
+        " 誘導影片課程，來了解軟體的核心概念以及入門，並跟著範例練習以了解其中之應用方式",
+    },
+    s2: {
+      name: "良性互動",
+      contents:
+        "學習過程中，若您得到任何問題或是您有任何想法都歡迎提出，我們會盡量的協助問題的解決",
+    },
+    s3: {
+      name: "趨勢分享",
+      contents: "分享目前時下所流行之趨勢，讓各位可隨時掌握新趨勢的發展",
+    },
+  }
+const cardBox={
+c1:{
+item1:"Unity 5",
+item2:"GameSalad 2D遊戲製作",
+item3:"Google Web Designer",
+
+},
+c2:{
+  img:"./images/unity.jpg",
+img2:"./images/gamesalad.jpg",
+},
+c3:{
+
+}
+}
+
+  const{name,contents}=textBox;
+  
+
   return (
     <>
       <header>
@@ -11,24 +46,23 @@ function App() {
 
       <main>
         {/*文字區 */}
+
         <div className="textBlock">
           <div>
-            <h2>教學影音</h2>
+            <h2>{textBox.s1.name}</h2>
             <p>
-              誘導影片課程，來了解軟體的核心概念以及入門，
-              並跟著範例練習以了解其中之應用方式
+              {textBox.s1.contents}
             </p>
           </div>
           <div>
-            <h2>良性互動</h2>
+            <h2>{textBox.s2.name}</h2>
             <p>
-              學習過程中，若您得到任何問題或是您有任何想法都歡迎
-              提出，我們會盡量的協助問題的解決
+              {textBox.s2.contents}
             </p>
           </div>
           <div>
-            <h2>趨勢分享</h2>
-            <p>分享目前時下所流行之趨勢，讓各位可隨時掌握新趨勢的發展</p>
+            <h2>{textBox.s3.name}</h2>
+            <p>{textBox.s3.contents}</p>
           </div>
         </div>
 
@@ -45,11 +79,16 @@ function App() {
               <img src="./images/unity.jpg" alt="" />
 
               <h2>Unity 5</h2>
-              <p className="p1">最新的Unity公開課程，讓你一步一步做出自己的心中理想的遊戲</p>
+              <p className="p1">
+                最新的Unity公開課程，讓你一步一步做出自己的心中理想的遊戲
+              </p>
               <p>講師:xxx</p>
               <p>影音課程時數:4小時</p>
-              <p><span className="span1">原價NT$1600</span><span className="span2">NT$1200</span></p>
-            
+              <p>
+                <span className="span1">原價NT$1600</span>
+                <span className="span2">NT$1200</span>
+              </p>
+
               <button>付款上課去</button>
             </div>
 
@@ -57,10 +96,15 @@ function App() {
               <img src="./images/gamesalad.jpg" alt="" />
 
               <h2>GameSalad 2D遊戲製作</h2>
-              <p className="p1">視覺化操作，2D遊戲快速開發，不會寫程式的你一樣可以寫遊戲</p>
+              <p className="p1">
+                視覺化操作，2D遊戲快速開發，不會寫程式的你一樣可以寫遊戲
+              </p>
               <p>講師:xxx</p>
               <p>影音課程時數:8小時</p>
-              <p><span className="span1">原價NT$3200</span><span className="span2">NT$1600</span></p>
+              <p>
+                <span className="span1">原價NT$3200</span>
+                <span className="span2">NT$1600</span>
+              </p>
               <button>付款上課去</button>
             </div>
             <div className="content">
@@ -73,7 +117,10 @@ function App() {
               </p>
               <p>講師:xxx</p>
               <p>影音課程時數:8小時</p>
-              <p><span className="span1">原價NT$3200</span><span className="span2">NT$1600</span></p>
+              <p>
+                <span className="span1">原價NT$3200</span>
+                <span className="span2">NT$1600</span>
+              </p>
               <button>付款上課去</button>
             </div>
           </div>
