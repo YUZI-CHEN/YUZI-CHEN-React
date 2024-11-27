4b3226fb840b5902be33ce7d18ec39ac823a8b68
yuxzi <kitty841002@gmail.com>
2024-11-27 15:24:22 +0800

    單一核取方塊+多個核取方塊

--- my-react-app1/src/App1127-3.jsx
+++ my-react-app1/src/App1127-3.jsx
@@ -8,7 +8,35 @@ function App() {
   // 陣列的寫法
   const [selCity, setSelCity] = useState('');
   //建立陣列
-  const listCity = ['台北市','新北市','桃園市','台中市', '台南市', '高雄市', ]
+  const listCity = ['台北市', '新北市', '桃園市', '台中市', '台南市', '高雄市',]
+  const  arrList = ['html','js','css']
+
+  // 閱讀確認
+  const [check, setCheck] = useState(false);
+
+  // 複選:多個核取方塊 
+  const [chkList, setChkList] = useState([]);
+
+  // 建立函式處理被勾選的項目
+  const handleChkList = (e) => {
+    // console.log(e.target.value)
+    // 使用其餘運算子，保留之前已經被勾選的項目+目前被勾選的項目
+    // setChkList([...chkList,e.target.value]);
+    // 檢查是否被勾選
+
+    if (e.target.checked) {
+      // 是=>原本+新的
+      setChkList([...chkList, e.target.value]);
+    } else {
+      // 否=>從原本被勾選的項目中，拿掉不要的(filter)
+      setChkList(
+        chkList.filter((list) => {
+          return list !== e.target.value;
+        })
+      )
+
+    }
+  }
   return (
     <>
       <h1>react-表單</h1>
@@ -57,6 +85,40 @@ function App() {
 
 
       </select>{selCity}
+      <br />
+
+      {/* 單一核取方塊 */}
+      <label htmlFor="isCheck">已閱讀</label>
+      <input type="checkbox" id="isCheck"
+        value={check}
+        onClick={(e) => {
+          //處理勾選值得變化
+          setCheck(e.target.check);
+
+        }} />{check.toString()}
+      <br />
+      {/* 複選:多個核取方塊 */}
+      {
+        arrList.map((list) => {
+          return <div key={list} >
+            <input type="checkbox" id={list} className="like"
+              value={list} onChange={handleChkList} />
+            <label htmlFor={list}>{list}</label>
+          </div>
+        })
+      }
+
+      {/* <input type="checkbox" id="chkList1" className="like" value='HTML' onChange={handleChkList} />
+      <label htmlFor="chkList1">HTML</label>
+
+      <input type="checkbox" id="chkList2" className="like" value='CSS' onChange={handleChkList} />
+      <label htmlFor="chkList2">CSS</label>
+
+      <input type="checkbox" id="chkList3" className="like" value='JS' onChange={handleChkList} />
+      <label htmlFor="chkList3">JS</label> */}
+      {chkList}
+
+
 
     </>
   )
