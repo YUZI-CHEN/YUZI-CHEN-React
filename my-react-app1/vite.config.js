8971dc534be7c06cc383aa4ffaf0318a52e4a7b1
yuxzi <kitty841002@gmail.com>
2024-12-03 11:52:23 +0800

    1203-2

--- my-react-app1/vite.config.js
+++ my-react-app1/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  bass:process.env.NODE_ENV==='production'?'/YUZI-CHEN-React/':'/',
+  base:process.env.NODE_ENV==='production'?'/YUZI-CHEN-React/':'/',
   plugins: [react()],
 })
