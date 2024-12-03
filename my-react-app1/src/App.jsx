68f2d3afeb27132b0da95d7a976ac7c6f5834a55
yuxzi <kitty841002@gmail.com>
2024-12-03 09:57:48 +0800

    網址

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,59 +1,200 @@
+import { useEffect } from "react";
+import axios from 'axios';
 import "./app.css";
-import ShowContent from "./ShowContent.jsx";
-import ShowCard from "./ShowCard.jsx";
-
+import { FaUmbrella } from "react-icons/fa6";
 function App() {
- 
 
-  
+  useEffect(() => {
+
+    (async () => {
+      // json檔案要放在雲端上
+      const data = await axios.get('https://yuzi-chen.github.io/https---github.com-YUZI-CHEN-React/json/F-C0032-001.json/');
+      console.log(data);
+      //第一個data是上一行的data
+
+      // const { location } = data.data.cwaopendata.dataset;
+      // console.log(location);
+    })()
+  }, [])
+
+
+
+
 
   return (
     <>
-      <header>
-        <div>
-          <img src="./images/banner.jpg" />
-        </div>
-      </header>
+      <div >
+        <h2>36小時天氣預報</h2>
+        {/* 一列二欄 */}
+        <div className="row">
+          <div className="col">
+            {/* 卡片樣式 */}
+            <div className="card">
+              {/* 標題 */}
+              <div className="card-title">
+                台北市
 
-      <main>
-        {/*文字區 */}
+              </div>
+              {/* 內容 */}
+              <div className="card-body">
+                {/* 一列三欄 */}
+                <div className="row2">
+                  <div className="col2">
+                    <p>2日</p>
+                    <p>
+                      上午6:00
+                      <br>
 
-        <div className="textBlock">
-          <ShowContent  />
-          {/* 方法一 */}
-          {/* {textBox.map((item) => {
-            return (
-              <>
-                <div key={item.id}>
-                  <h2>{item.name}</h2>
-                  <p>{item.contents}</p>
+                      </br>下午6:00
+                    </p>
+                    <p>
+                      <img src="./public/weatherIcon/晴時多雲.svg" alt="" />
+                    </p>
+                    <p>晴時多雲</p>
+                    <p>{<FaUmbrella />} 10%</p>
+
+                  </div>
                 </div>
-              </>
-            );
-          })} */}
-        </div>
+                <div className="row2">
+                  <div className="col2">
+                    <p>2日</p>
+                    <p>
+                      上午6:00
+                      <br>
 
-        {/* 主標 */}
-        <div className="item">
-          <h1>讓學習是一種習慣</h1>
-        </div>
+                      </br>下午6:00
+                    </p>
+                    <p>
+                      <img src="./public/weatherIcon/晴時多雲.svg" alt="" />
+                    </p>
+                    <p>晴時多雲</p>
+                    <p>{<FaUmbrella />} 10%</p>
 
-        {/* 課程 */}
-        <div className="class">
-          <h2>推薦課程</h2>
-          <div className="classBox">
-            <ShowCard  />
+                  </div>
+                </div>
+                <div className="row2">
+                  <div className="col2">
+                    <p>2日</p>
+                    <p>
+                      上午6:00
+                      <br>
+
+                      </br>下午6:00
+                    </p>
+                    <p>
+                      <img src="./public/weatherIcon/晴時多雲.svg" alt="" />
+                    </p>
+                    <p>晴時多雲</p>
+                    <p>{<FaUmbrella />} 10%</p>
+
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </main>
+          <div className="col">
+            {/* 卡片樣式 */}
+            <div className="card">
+              {/* 標題 */}
+              <div className="card-title">
+                台北市
 
-      <footer>
-        <div>
-          <p>&copy;2024/11/12陳姿佑</p>
+              </div>
+              {/* 內容 */}
+              <div className="card-body">
+                {/* 一列三欄 */}
+                <div className="row2">
+                  <div className="col2">
+                    <p>2日</p>
+                    <p>
+                      上午6:00
+                      <br>
+
+                      </br>下午6:00
+                    </p>
+                    <p>
+                      <img src="./public/weatherIcon/晴時多雲.svg" alt="" />
+                    </p>
+                    <p>晴時多雲</p>
+                    <p>{<FaUmbrella />} 10%</p>
+
+                  </div>
+                </div>
+                <div className="row2">
+                  <div className="col2">
+                    <p>2日</p>
+                    <p>
+                      上午6:00
+                      <br>
+
+                      </br>下午6:00
+                    </p>
+                    <p>
+                      <img src="./public/weatherIcon/晴時多雲.svg" alt="" />
+                    </p>
+                    <p>晴時多雲</p>
+                    <p>{<FaUmbrella />} 10%</p>
+
+                  </div>
+                </div>
+                <div className="row2">
+                  <div className="col2">
+                    <p>2日</p>
+                    <p>
+                      上午6:00
+                      <br>
+
+                      </br>下午6:00
+                    </p>
+                    <p>
+                      <img src="./public/weatherIcon/晴時多雲.svg" alt="" />
+                    </p>
+                    <p>晴時多雲</p>
+                    <p>{<FaUmbrella />} 10%</p>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+       
         </div>
-      </footer>
+        
+          
+      </div>
+
+      {/* <div className="card" >
+        <div className="item">
+          <p>臺北市</p>
+        </div>
+        <div className="counter">
+          <p>2日</p>
+          <p>上午6:00~下午6:00</p>
+          <img src="" alt="" />
+          <p>晴時多雲</p>
+          <p>10%</p>
+        </div>
+        <div className="counter" >
+          <p>2日</p>
+          <p>上午6:00~下午6:00</p>
+          <img src="" alt="" />
+          <p>多雲時晴</p>
+          <p>20%</p>
+        </div>
+        <div className="counter">
+          <p>3日</p>
+          <p>上午6:00~下午6:00</p>
+          <img src="" alt="" />
+          <p>多雲短暫雨 </p>
+          <p>30%</p>
+        </div>
+
+
+      </div>
+ */}
+
     </>
-  );
+  )
 }
 
 export default App;
--- my-react-app1/vite.config.js
+++ my-react-app1/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  bass:process.env.NOGE_ENV==='production'?'/https---github.com-YUZI-CHEN-React/':'/',
+
   plugins: [react()],
 })
