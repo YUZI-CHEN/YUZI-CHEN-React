41002da87fd598cceac63401c7c648723ac3d4b0
yuxzi <kitty841002@gmail.com>
2024-12-04 14:27:35 +0800

    weather天氣變化1204

---
+++ my-react-app1/src/App-1204.jsx
@@ -0,0 +1,100 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import "./app.css";
+import WeatherDay from "./component/WeatherDay";
+
+
+
+
+
+
+function App() {
+  const [weatherList, setWeatherList] = useState([]);
+
+  useEffect(() => {
+
+    (async () => {
+      // json檔案要放在雲端上
+      const data = await axios.get('https://yuzi-chen.github.io/YUZI-CHEN-React/json/F-C0032-001.json');
+      // console.log(data);
+      //第一個data是上一行的data
+
+      const { location } = data.data.cwaopendata.dataset;
+      setWeatherList(location);
+      console.log(location);
+
+
+      //locationName=>縣市名稱
+      // elementName=>Wx=>天氣概況
+      // elementName=>PoP=>降雨機率
+
+
+    })()
+  }, [])
+
+
+
+  return (
+    <>
+      <div className="wrap">
+        <h2>36小時天氣預報</h2>
+        {/* 一列二欄 */}
+        <div className="container">
+          {
+
+            weatherList.map((city) => {
+              return (
+                 < div className = "item" key = { city.locationName } >
+
+                <h3>{city.locationName}</h3>
+                {/* 要傳送city給WeatherDay*/}
+                <WeatherDay city={city} />
+                </div>
+              )
+            })
+          }
+
+
+
+        </div>
+
+
+
+
+      </div>
+
+      {/* <div className="card" >
+        <div className="item">
+          <p>臺北市</p>
+        </div>
+        <div className="counter">
+          <p>2日</p>
+          <p>上午6:00~下午6:00</p>
+          <img src="" alt="" />
+          <p>晴時多雲</p>
+          <p>10%</p>
+        </div>
+        <div className="counter" >
+          <p>2日</p>
+          <p>上午6:00~下午6:00</p>
+          <img src="" alt="" />
+          <p>多雲時晴</p>
+          <p>20%</p>
+        </div>
+        <div className="counter">
+          <p>3日</p>
+          <p>上午6:00~下午6:00</p>
+          <img src="" alt="" />
+          <p>多雲短暫雨 </p>
+          <p>30%</p>
+        </div>
+
+
+      </div>
+ */}
+
+    </>
+  )
+}
+
+export default App;
