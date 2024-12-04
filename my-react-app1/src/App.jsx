4c944ce2bf78d154276eb5c71afb0e2089a59590
yuxzi <kitty841002@gmail.com>
2024-12-04 09:36:22 +0800

    1204修改

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from 'axios';
 import "./app.css";
 import { FaUmbrella } from "react-icons/fa6";
@@ -7,19 +7,53 @@ import { FaUmbrella } from "react-icons/fa6";
 
 
 function App() {
+  const [weatherList, setWeatherList] = useState([]);
 
-  useEffect(() => {}, [])
+  useEffect(() => {
 
-   
+    (async () => {
+      // json檔案要放在雲端上
+      const data = await axios.get('https://yuzi-chen.github.io/YUZI-CHEN-React/json/F-C0032-001.json/');
+      // console.log(data);
+      //第一個data是上一行的data
+
+      const { location } = data.data.cwaopendata.dataset;
+      console.log(location);
+
+
+      //locationName=>縣市名稱
+      // elementName=>Wx=>天氣概況
+      // elementName=>PoP=>降雨機率
+
+
+    })()
+  }, [])
+
+
 
   return (
     <>
       <div >
         <h2>36小時天氣預報</h2>
         {/* 一列二欄 */}
+        <div className="container">
+          {
+            weatherList.map((city) => {
+              return <div className="item" key={city.locationName}>
+
+                <h3>{city.locationName}</h3>
+
+
+              </div>
+
+
+            })
+          }
+        </div>
+
         <div className="row">
 
-          <Weather/>
+
         </div>
 
 
