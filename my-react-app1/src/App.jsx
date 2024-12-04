e1dbb321c5e679d638cb4760a4c0541205edd4c6
yuxzi <kitty841002@gmail.com>
2024-12-04 14:25:31 +0800

    變成元件弄到WeatherDay

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 import "./app.css";
-import { FaUmbrella } from "react-icons/fa6";
+import WeatherDay from "./component/WeatherDay";
+
+
 
 
 
@@ -39,91 +41,24 @@ function App() {
         {/* 一列二欄 */}
         <div className="container">
           {
+
             weatherList.map((city) => {
-              return <div className="item" key={city.locationName}>
+              return (
+                 < div className = "item" key = { city.locationName } >
 
                 <h3>{city.locationName}</h3>
-                <div className="content">
-                  {
-
-                    city.weatherElement[0].time.map((time, index) => {
-                      return (
-                        <div className="item2" key={index}>
-                          {/* <p>4日</p> */}
-                          <p>
-                            {
-                              /* 擷取日期 */
-                              new Date(time. startTime).toLocaleString(undefined,
-                                { day: 'numeric' }
-                              )
-
-                            }
-                          </p>
-                          <p>
-                            {/* 時間 */}
-                            {
-                              new Date(time. startTime).toLocaleString(undefined, {
-                                hour: 'numeric',
-                                minute: 'numeric'
-                              })
-                            }
-                            <br />~<br />
-                            {
-                              new Date(time. endTime).toLocaleString(undefined, {
-                                hour: 'numeric',
-                                minute: 'numeric'
-                              })
-                            }
-                          </p>
-                          {/* 天氣 */}
-                          {/* <img src="../public/weatherIcon/晴時多雲.svg" alt="" /> */}
-                          <p><img src={`./weatherIcon/${time.parameter.parameterName}.svg`} alt="" /></p>
-                          <p>{time.parameter.parameterName}</p>
-
-                          {/* 降雨機率 */}
-
-                          {/* <p>{<FaUmbrella />} 30% </p> */}
-                          <p><FaUmbrella />
-                          {
-                            city.weatherElement[4].time[index].parameter.parameterName
-                          }
-
-                           </p>
-                           <p>
-                           {
-                            city.weatherElement[4].time[index].parameter.parameterUnit
-                          }
-                           </p>
-
-                        </div>
-
-                      )
-
-                    })
-
-
-
-
-
-
-                  }
-
-
-
+                {/* 要傳送city給WeatherDay*/}
+                <WeatherDay city={city} />
                 </div>
-
-
-              </div>
-
-
+              )
             })
           }
-        </div>
 
-        <div className="row">
 
 
         </div>
+
+
 
 
       </div>
