9236d7c8c8629025d7497e095718e6af4146b673
yuxzi <kitty841002@gmail.com>
2024-12-04 16:09:05 +0800

    react-navbar1204

---
+++ my-react-app1/src/component/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+
+    <div>
+      <ul>
+
+        <li>
+          <Link to='/'>首頁</Link>
+        </li>
+        <li>
+          <Link to='/about'>關於我</Link>
+        </li>
+        <li>
+          <Link to='/new'>最新消息</Link>
+        </li>
+      </ul>
+    </div>
+
+  )
+
+} 
