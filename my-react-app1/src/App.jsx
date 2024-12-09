a363b9b86e8b9fb93766cbfe2b0802ee1aff2d3e
yuxzi <kitty841002@gmail.com>
2024-12-09 11:51:29 +0800

    1209jQuary練習(顏色、大小、位置、動畫)

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,100 +1,34 @@
-import { useEffect, useState } from "react";
-import axios from 'axios';
-import "./app.css";
-import WeatherDay from "./component/WeatherDay";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import New from "./pages/New";
 
 
 
-
-
-
-function App() {
-  const [weatherList, setWeatherList] = useState([]);
-
-  useEffect(() => {
-
-    (async () => {
-      // json檔案要放在雲端上
-      const data = await axios.get('https://yuzi-chen.github.io/YUZI-CHEN-React/json/F-C0032-001.json');
-      // console.log(data);
-      //第一個data是上一行的data
-
-      const { location } = data.data.cwaopendata.dataset;
-      setWeatherList(location);
-      console.log(location);
-
-
-      //locationName=>縣市名稱
-      // elementName=>Wx=>天氣概況
-      // elementName=>PoP=>降雨機率
-
-
-    })()
-  }, [])
-
-
-
+export default function App() {
   return (
-    <>
-      <div className="wrap">
-        <h2>36小時天氣預報</h2>
-        {/* 一列二欄 */}
-        <div className="container">
-          {
-
-            weatherList.map((city) => {
-              return (
-                 < div className = "item" key = { city.locationName } >
-
-                <h3>{city.locationName}</h3>
-                {/* 要傳送city給WeatherDay*/}
-                <WeatherDay city={city} />
-                </div>
-              )
-            })
-          }
 
 
-
-        </div>
+    <div className="wrap">
+      <h1>導覽列</h1>
+      {/* <Navbar/> */}
+      <Navbar />
+      <hr />
+      {/* <Routes>
+        <Route  path="/" element={<Home/>}></Route>
+        <Route  path="/about" element={<About/>}></Route>
+        <Route  path="/news" element={<New/>}></Route>
+      </Routes> */}
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/new" element={<New/>}></Route>
+      </Routes>
 
 
 
+    </div>
 
-      </div>
-
-      {/* <div className="card" >
-        <div className="item">
-          <p>臺北市</p>
-        </div>
-        <div className="counter">
-          <p>2日</p>
-          <p>上午6:00~下午6:00</p>
-          <img src="" alt="" />
-          <p>晴時多雲</p>
-          <p>10%</p>
-        </div>
-        <div className="counter" >
-          <p>2日</p>
-          <p>上午6:00~下午6:00</p>
-          <img src="" alt="" />
-          <p>多雲時晴</p>
-          <p>20%</p>
-        </div>
-        <div className="counter">
-          <p>3日</p>
-          <p>上午6:00~下午6:00</p>
-          <img src="" alt="" />
-          <p>多雲短暫雨 </p>
-          <p>30%</p>
-        </div>
-
-
-      </div>
- */}
-
-    </>
   )
 }
-
-export default App;
