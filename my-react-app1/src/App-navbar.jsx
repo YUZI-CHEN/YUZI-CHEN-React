8bc14a964236a4299b774d9f4eef2ee653824bb5
yuxzi <kitty841002@gmail.com>
2024-12-10 11:26:10 +0800

    淡入淡出輪播

---
+++ my-react-app1/src/App-navbar.jsx
@@ -0,0 +1,34 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import New from "./pages/New";
+
+
+
+export default function App() {
+  return (
+
+
+    <div className="wrap">
+      <h1>導覽列</h1>
+      {/* <Navbar/> */}
+      <Navbar />
+      <hr />
+      {/* <Routes>
+        <Route  path="/" element={<Home/>}></Route>
+        <Route  path="/about" element={<About/>}></Route>
+        <Route  path="/news" element={<New/>}></Route>
+      </Routes> */}
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/new" element={<New />}></Route>
+      </Routes>
+
+
+
+    </div>
+
+  )
+}
--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,34 +1,19 @@
-import { Route, Routes } from "react-router-dom";
-import Navbar from "./component/Navbar";
-import About from "./pages/About";
-import Home from "./pages/Home";
-import New from "./pages/New";
-
+import $ from 'jquery'//在react使用jQuery
 
 
 export default function App() {
   return (
-
-
-    <div className="wrap">
-      <h1>導覽列</h1>
-      {/* <Navbar/> */}
-      <Navbar />
-      <hr />
-      {/* <Routes>
-        <Route  path="/" element={<Home/>}></Route>
-        <Route  path="/about" element={<About/>}></Route>
-        <Route  path="/news" element={<New/>}></Route>
-      </Routes> */}
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/about" element={<About/>}></Route>
-        <Route path="/new" element={<New/>}></Route>
-      </Routes>
-
-
-
+    <div>
+     <h2>jquery</h2>
+    <button onClick={()=>
+    {console.log($('h2').text())}}>
+      click</button>
     </div>
 
+
+    
+
+
+   
   )
 }
