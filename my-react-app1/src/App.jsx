84a2a92fa9ece02365f1e5c519fa174494676a3b
yuxzi <kitty841002@gmail.com>
2024-12-10 15:47:18 +0800

    使用react寫出動畫addClass(),removeClass()

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,19 +1,49 @@
 import $ from 'jquery'//在react使用jQuery
+import { useEffect } from 'react';
 
 
 export default function App() {
+  // 方法二
+
+
+  // 結構渲染完畢，在執行所以要使用useEffect
+  useEffect(() => {
+    $('.cssAnima').on('mouseover', function () {
+      $(this).addClass('imgScale');
+    })
+
+
+    $('.cssAnima').on('mouseout', function () {
+      $(this).removeClass('imgScale');
+    })
+
+  }, [])
+  // 方法一(過時)
+  // useEffect(() => {
+  //   $('.cssAnima').hover(function(){
+  //     $(this).addClass('imgScale')
+  // },function(){
+  //   $(this).removeClass('imgScale')
+  // }) 
+
+  // }, [])
+
+
+
   return (
-    <div>
-     <h2>jquery</h2>
-    <button onClick={()=>
-    {console.log($('h2').text())}}>
-      click</button>
+    <div className="wrapper">
+      <h2>動態新增刪除類別</h2>
+      <ul>
+        <li>新增類別=>addClass()</li>
+        <li>移除類別=>removeClass()</li>
+      </ul>
+      <hr />
+      <a href="#" class="cssAnima">
+        <img src="./image/02.jpg" alt="" />
+      </a>
     </div>
 
-
-    
-
-
-   
   )
 }
+
+
