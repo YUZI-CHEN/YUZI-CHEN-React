b29cdcff49f6eb8d3a72e0e845c2fd95f9444810
yuxzi <kitty841002@gmail.com>
2024-12-16 09:44:24 +0800

    改變網頁路徑變成相對位置

--- my-react-app1/vite.config.js
+++ my-react-app1/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:process.env.NODE_ENV==='production'?'/YUZI-CHEN-React/':'/',
+  // base:process.env.NODE_ENV==='production'?'/YUZI-CHEN-React/':'/',
+  base:'./',
   plugins: [react()],
 })
