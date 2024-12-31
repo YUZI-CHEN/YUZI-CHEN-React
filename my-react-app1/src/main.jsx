d4d8762652757bb7685523ba39069bd728971f20
yuxzi <kitty841002@gmail.com>
2024-12-31 11:35:10 +0800

    還原回App顯示

--- my-react-app1/src/main.jsx
+++ my-react-app1/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-import  AppNavbar from './AppNavbar.jsx'
+import App from './App.jsx'
+// import  AppNavbar from './AppNavbar.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './assets/all.scss'
 import './app.css'
@@ -10,8 +10,8 @@ import './app.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>  
     <BrowserRouter>
-    {/* <App /> */}
-    <AppNavbar/>
+    <App />
+    {/* <AppNavbar/> */}
     </BrowserRouter>      
    
   </React.StrictMode>,
